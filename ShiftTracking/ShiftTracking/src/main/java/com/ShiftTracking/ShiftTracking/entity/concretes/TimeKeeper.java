4690a90448780a0b10d9c126568f2143f46fe32f
Ali Berke Geçgel <47008493+aliberke1910@users.noreply.github.com>
2023-03-18 22:40:55 +0300

    Add files via upload

---
+++ ShiftTracking/ShiftTracking/src/main/java/com/ShiftTracking/ShiftTracking/entity/concretes/TimeKeeper.java
@@ -0,0 +1,14 @@
+package com.ShiftTracking.ShiftTracking.entity.concretes;
+
+import java.time.LocalDateTime;
+
+public class TimeKeeper {
+	
+	public void readTime()
+	
+	{
+        LocalDateTime.now();
+      
+    }
+
+}
